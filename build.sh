31f633b427945b4ccf32ded8a85254ebd38a96fb
廖小白 <Goatman914@gmail.com>
2025-06-23 23:38:44 +0800

    first window

---
+++ build.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+if [ ! -d "build" ]; then
+  mkdir build
+fi
+
+cd ./build
+cmake ..
+make 
+
+echo ========================
+echo "Build Done!"
+echo ========================
+
+cd ..
+cd ./bin
+./LearnOpenGL
